f3d101d2b747af2a96216f281c908563d80162e1
MyNameWasUnavailable <keith@parker-rolls.com>
2026-07-08 19:27:33 +1000

    Fix profiledef.sh: add all required variables for archiso 88

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 # archiso profile definition for Dell Venue 8 Pro 5830 automated install
 
-profile_title="Arch Linux - Dell Venue 8 Pro 5830"
-profile_description="Automated Plasma Mobile install for Dell Venue 8 Pro with all hardware fixes integrated"
-profile_url="https://github.com/ramonvanraaij/dell-venue-8-pro"
-compression="xz"
-compress_options=('-C' 'check=crc32' '-9' '-e')
+iso_name="arch-venue-8-pro"
+iso_label="ARCH_VENUE8PRO_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="MyNameWasUnavailable <https://github.com/MyNameWasUnavailable/dell-venue-8-pro-iso>"
+iso_application="Arch Linux - Dell Venue 8 Pro 5830 Automated Installer"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+airootfs_image_type="squashfs"
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
+bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 
-Validate_signature_option_value() {
-    return 0
-}
-
-Validate_profile_conf() {
-    return 0
-}
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+  ["/root"]="0:0:750"
+  ["/root/.automated_script.sh"]="0:0:755"
+  ["/usr/local/sbin"]="0:0:755"
+  ["/usr/local/sbin/ath6kl-tune.sh"]="0:0:755"
+  ["/usr/local/sbin/arch-launcher-icon.sh"]="0:0:755"
+  ["/usr/local/sbin/venue-batfix-build.sh"]="0:0:755"
+  ["/root/install-venue.sh"]="0:0:755"
+)
